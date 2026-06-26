49cbc050af73f504a6320edc63b433b62b0e94e5
SIREESHA-BOMMISETTY <bsireesha162@gmail.com>
2026-06-26 12:08:46 +0530

    add input_errors.py

---
+++ input_errors.py
@@ -0,0 +1,13 @@
+num= input("Enter Number:")
+print(num + 10)
+
+num=int(input("Enter Number:"))
+print(num + 10)
+
+# Missing Comma in print()
+
+name = "sandy"
+print("Name:",name)
+
+name = "sandy"
+print("Name",name)
